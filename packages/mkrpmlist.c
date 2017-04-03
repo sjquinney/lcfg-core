d883af286b3f64cb3043a5dba24e4e76a317120f
squinney@INF.ED.AC.UK <squinney@INF.ED.AC.UK@c6336a39-18d7-4234-9af0-a2bff563b40a>
2017-04-03 11:08:22 +0000

    Example of how to generate an rpmlist file from a directory of RPMs
    git-svn-id: https://svn.lcfg.org/svn/source/trunk/lcfg-core@32187 c6336a39-18d7-4234-9af0-a2bff563b40a

---
+++ packages/mkrpmlist.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <lcfg/packages.h>
+
+int main(int argc, char *argv[]) {
+
+  const char * rpmdir  = argv[1];
+  const char * outfile = argv[2];
+
+  LCFGPackageList * pkglist = NULL;
+  char * msg = NULL;
+
+  LCFGStatus read_rc = lcfgpkglist_from_rpm_dir( rpmdir, &pkglist, &msg );
+
+  if ( read_rc == LCFG_STATUS_ERROR ) {
+    fprintf( stderr, "Failed to read rpm directory '%s': %s\n", rpmdir, msg );
+  } else {
+
+    LCFGStatus write_rc = lcfgpkglist_to_rpmlist( pkglist,
+                                                  NULL,
+                                                  outfile, 
+                                                  0,
+                                                  &msg );
+
+    if ( write_rc == LCFG_STATUS_ERROR )
+      fprintf( stderr, "Failed to write rpmlist '%s': %s\n", outfile, msg );
+
+  }
+
+  lcfgpkglist_destroy(pkglist);
+  free(msg);
+
+  return 0;
+}
