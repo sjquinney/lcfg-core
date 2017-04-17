9890b395eb19f1529a89fa54cd1d584274d477b3
squinney@INF.ED.AC.UK <squinney@INF.ED.AC.UK@c6336a39-18d7-4234-9af0-a2bff563b40a>
2017-04-17 15:27:11 +0000

    Reworked to have an API which is consistent which the rest of the code base
    git-svn-id: https://svn.lcfg.org/svn/source/trunk/lcfg-core@32359 c6336a39-18d7-4234-9af0-a2bff563b40a

--- include/templates.h
+++ include/templates.h
@@ -18,9 +18,21 @@ struct LCFGTemplate {
 
 typedef struct LCFGTemplate LCFGTemplate;
 
-LCFGTemplate * lcfgtemplate_new( char * name, char * tmpl, char ** msg );
+LCFGTemplate * lcfgtemplate_new(void);
 
 void lcfgtemplate_destroy(LCFGTemplate * head_template);
+
+bool lcfgtemplate_is_valid ( const LCFGTemplate * template );
+
+char * lcfgtemplate_get_name( const LCFGTemplate * template );
+bool lcfgtemplate_set_name( LCFGTemplate * template, char * new_name )
+  __attribute__((warn_unused_result));
+
+bool lcfgresource_valid_template( const char * tmpl );
+
+char * lcfgtemplate_get_tmpl( LCFGTemplate * template );
+bool lcfgtemplate_set_tmpl( LCFGTemplate * template, char * new_tmpl )
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgtemplate_from_string( const char * input,
                                      LCFGTemplate ** template,
@@ -31,10 +43,9 @@ ssize_t lcfgtemplate_to_string( const LCFGTemplate * head_template,
                                 const char * prefix,
                                 LCFGOption options,
                                 char ** str, size_t * len )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGTemplate * lcfgtemplate_find( const LCFGTemplate * head_template,
-                                  const char * field_name )
-  __attribute__((nonnull (1,2)));
+                                  const char * field_name );
 
 /* eof */
--- resources/templates.c
+++ resources/templates.c
@@ -1,14 +1,38 @@
 #define _GNU_SOURCE /* for asprintf */
 
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <errno.h>
+#include <ctype.h>
 
 #include "templates.h"
+#include "utils.h"
 
-LCFGTemplate * lcfgtemplate_new( char * name, char * tmpl, char ** msg ) {
+static LCFGStatus invalid_template( char ** msg, const char * base, ... ) {
+
+  const char * fmt = "Invalid template (%s)";
+
+  va_list ap;
+  va_start( ap, base );
+
+  char * reason = NULL;
+  int rc = vasprintf( &reason, base, ap );
+  if ( rc < 0 ) {
+    perror("Failed to allocate memory for error string");
+    exit(EXIT_FAILURE);
+  }
+
+  lcfgutils_build_message( msg, fmt, reason );
+  free(reason);
+
+  return LCFG_STATUS_ERROR;
+}
+
+LCFGTemplate * lcfgtemplate_new(void) {
 
   LCFGTemplate * template = malloc( sizeof(LCFGTemplate) );
   if ( template == NULL ) {
@@ -16,12 +40,10 @@ LCFGTemplate * lcfgtemplate_new( char * name, char * tmpl, char ** msg ) {
     exit(EXIT_FAILURE);
   }
 
-  /* Defaults */
-
-  template->name     = name;
-  template->name_len = name != NULL ? strlen(name) : 0;
-  template->tmpl     = tmpl;
-  template->tmpl_len = tmpl != NULL ? strlen(tmpl) : 0;
+  template->name     = NULL;
+  template->name_len = 0;
+  template->tmpl     = NULL;
+  template->tmpl_len = 0;
 
   int i;
   for ( i=0; i<LCFG_TAGS_MAX_DEPTH; i++ )
@@ -31,47 +53,12 @@ LCFGTemplate * lcfgtemplate_new( char * name, char * tmpl, char ** msg ) {
 
   template->next = NULL;
 
-  /* The template MUST be at least one character long */
-
-  bool valid = ( tmpl != NULL && *tmpl != '\0' );
-
-  /* Use array to cache the locations of the placeholders. Avoids
-     having to find them multiple times when actually building the
-     resource names. */
-
-  /* Note that this works **backwards** from the tail of the list to
-     the head. */
-
-  if (valid) {
-
-    for ( i=template->tmpl_len - 1; i >= 0; i-- ) {
-      if ( tmpl[i] == LCFG_TEMPLATE_PLACEHOLDER ) {
-
-        template->pcount++;
-        if ( template->pcount > LCFG_TAGS_MAX_DEPTH ) {
-          asprintf( msg, "Too many placeholders (%d).\n", template->pcount );
-          valid = false;
-          break;
-        }
-
-        template->places[template->pcount - 1] = i;
-      }
-    }
-
-  }
-
-  if ( !valid ) {
-    free(template);
-    template = NULL;
-  }
-
   return template;
 }
 
 void lcfgtemplate_destroy(LCFGTemplate * head_template) {
 
-  if ( head_template == NULL )
-    return;
+  if ( head_template == NULL ) return;
 
   LCFGTemplate * cur_template = head_template;
   LCFGTemplate * next_template;
@@ -91,36 +78,143 @@ void lcfgtemplate_destroy(LCFGTemplate * head_template) {
 
 }
 
+bool lcfgtemplate_is_valid ( const LCFGTemplate * template ) {
+  return ( template       != NULL &&
+           template->name != NULL &&
+           template->tmpl != NULL );
+}
+
+char * lcfgtemplate_get_name( const LCFGTemplate * template ) {
+  assert( template != NULL );
+  return template->name;
+}
+
+bool lcfgtemplate_set_name( LCFGTemplate * template, char * new_name ) {
+  assert( template != NULL );
+
+  /* Must be a valid 'resource' name */
+
+  bool ok = false;
+  if ( lcfgresource_valid_name(new_name) ) {
+    free(template->name);
+
+    template->name     = new_name;
+    template->name_len = strlen(new_name);
+
+    ok = true;
+  } else {
+    errno = EINVAL;
+  }
+
+  return ok;
+}
+
+bool lcfgresource_valid_template( const char * tmpl ) {
+
+  /* Extends the permitted character set for a resource name by
+     including the '$' placeholder */
+
+  /* MUST NOT be a NULL.
+     MUST have non-zero length.
+     First character MUST be in [A-Za-z] set. */
+
+  bool valid = ( !isempty(tmpl) && isalpha(*tmpl) );
+
+  /* All other characters MUST be in [A-Za-z0-9_$] set */
+
+  int pcount = 0;
+
+  char * ptr;
+  for ( ptr = ((char *)tmpl) + 1; *ptr != '\0'; ptr++ ) {
+    if ( *ptr == LCFG_TEMPLATE_PLACEHOLDER ) {
+      if ( ++pcount > LCFG_TAGS_MAX_DEPTH ) valid = false;
+    } else if ( !isword(*ptr) ) {
+      valid = false;
+    }
+  }
+
+  /* Must be at least one placeholder */
+
+  if ( pcount < 1 ) valid = false;
+
+  return valid;
+}
+
+char * lcfgtemplate_get_tmpl( LCFGTemplate * template ) {
+  assert( template != NULL );
+  return template->tmpl;
+}
+
+bool lcfgtemplate_set_tmpl( LCFGTemplate * template, char * new_tmpl ) {
+
+  /* As well as being a valid template it must begin with the same
+     string as the template 'name' */
+
+  bool ok = false;
+  bool valid = ( lcfgresource_valid_template(new_tmpl) &&
+                 strncmp( new_tmpl, template->name, template->name_len ) == 0 );
+  if (valid) {
+    free(template->tmpl);
+
+    template->tmpl     = new_tmpl;
+    template->tmpl_len = strlen(new_tmpl);
+
+    /* Use an array to cache the locations of the placeholders. Avoids
+       having to find them multiple times when actually building the
+       resource names. */
+
+    int i;
+    for ( i=template->tmpl_len - 1; i >= 0; i-- ) {
+      if ( new_tmpl[i] == LCFG_TEMPLATE_PLACEHOLDER ) {
+        template->pcount++;
+        template->places[template->pcount - 1] = i;
+      }
+    }
+
+    ok = true;
+  } else {
+    errno = EINVAL;
+  }
+
+  return ok;
+}
+
 ssize_t lcfgtemplate_to_string( const LCFGTemplate * head_template,
                                 const char * prefix,
                                 LCFGOption options,
                                 char ** result, size_t * size ) {
 
-  LCFGTemplate * cur_template;
-
   /* Calculate the required space */
 
-  cur_template = (LCFGTemplate *) head_template;
-  size_t new_len = cur_template->tmpl_len;
+  size_t new_len = 0;
 
   size_t prefix_len = 0;
-  if ( prefix != NULL ) {
+  if ( !isempty(prefix) ) {
     prefix_len = strlen(prefix);
     new_len += prefix_len;
   }
 
-  /* All additional templates are added with an extra single space
-     separator. */
+  if ( head_template != NULL ) {
 
-  cur_template = cur_template->next;
-  while ( cur_template       != NULL &&
-          cur_template->tmpl != NULL &&
-          cur_template->tmpl_len > 0 ) {
+    /* Head */
 
-    new_len += ( 1 + cur_template->tmpl_len ); /* +1 for single space */
+    new_len += head_template->tmpl_len;
 
-    cur_template = cur_template->next;
+    /* Remainder of templates are added with an extra single space
+       separator. */
+
+    const LCFGTemplate * cur_template = NULL;
+    for ( cur_template = head_template->next;
+          cur_template != NULL;
+          cur_template = cur_template->next ) {
+
+      new_len += ( 1 + cur_template->tmpl_len ); /* +1 for single space */
+    }
+
   }
+
+  if ( options&LCFG_OPT_NEWLINE )
+    new_len += 1;
 
   /* Allocate the required space */
 
@@ -142,31 +236,34 @@ ssize_t lcfgtemplate_to_string( const LCFGTemplate * head_template,
 
   char * to = *result;
 
-  if ( prefix != NULL && prefix_len > 0 )
+  if ( prefix_len > 0 )
     to = stpncpy( to, prefix, prefix_len );
 
-  cur_template = (LCFGTemplate *) head_template;
+  if ( head_template != NULL ) {
 
-  if (  cur_template       != NULL &&
-	cur_template->tmpl != NULL &&
-	cur_template->tmpl_len > 0 ) {
-    to = stpncpy( to, cur_template->tmpl, cur_template->tmpl_len );
+    /* Head */
+
+    to = stpncpy( to, head_template->tmpl, head_template->tmpl_len );
+
+    /* Remainder of list */
+ 
+    const LCFGTemplate * cur_template = NULL;
+    for ( cur_template = head_template->next;
+          cur_template != NULL;
+          cur_template = cur_template->next ) {
+
+      /* +1 for single space */
+
+      *to = ' ';
+      to++;
+
+      to = stpncpy( to, cur_template->tmpl, cur_template->tmpl_len );
+    }
+
   }
 
-  cur_template = cur_template->next;
-  while ( cur_template       != NULL &&
-          cur_template->tmpl != NULL &&
-          cur_template->tmpl_len > 0 ) {
-
-    /* +1 for single space */
-
-    *to = ' ';
-    to++;
-
-    to = stpncpy( to, cur_template->tmpl, cur_template->tmpl_len );
-
-    cur_template = cur_template->next;
-  }
+  if ( options&LCFG_OPT_NEWLINE )
+    to = stpncpy( to, "\n", 1 );
 
   *to = '\0';
 
@@ -179,9 +276,10 @@ LCFGStatus lcfgtemplate_from_string( const char * input,
                                      LCFGTemplate ** result,
                                      char ** msg ) {
 
-  *result = NULL;
-  *msg    = NULL;
+  if ( isempty(input) )
+    return invalid_template( msg, "empty string" );
 
+  LCFGStatus status = LCFG_STATUS_OK;
   LCFGTemplate * head_template = NULL;
   LCFGTemplate * prev_template;
 
@@ -189,46 +287,53 @@ LCFGStatus lcfgtemplate_from_string( const char * input,
   char * saveptr;
   static const char * whitespace = " \n\r\t";
 
-  bool ok = true;
-
   char * token = strtok_r( remainder, whitespace, &saveptr );
-  while (token) {
+  while ( status == LCFG_STATUS_OK && token ) {
 
     /* Splitting on the first instance of the '_$' string */
-    char * split_at = strstr( token, "_$" );
+    const char * split_at = strstr( token, "_$" );
     if ( split_at == NULL ) {
-      ok = false;
-      asprintf( msg, "No placeholder found in template '%s'", token );
+      status = invalid_template( msg, "no placeholder in '%s'", token );
       break;
     }
 
+    LCFGTemplate * new_template = lcfgtemplate_new();
+
+    /* Stub resource name */
     char * newname = strndup(token, split_at - token);
-    char * newtmpl = strdup(token);
 
-    char * new_msg = NULL;
-    LCFGTemplate * new_template =
-      lcfgtemplate_new( newname, newtmpl, &new_msg );
-    if ( new_template == NULL ) {
-      ok = false;
-
-      if ( new_msg != NULL ) {
-        asprintf( msg, "Failed to parse template '%s': %s", newtmpl, new_msg );
-        free(new_msg);
-      } else {
-        asprintf( msg, "Failed to parse template '%s'", newtmpl );
-      }
-
+    if ( !lcfgtemplate_set_name( new_template, newname ) ) {
+      status = invalid_template( msg, "bad resource name '%s'", newname );
       free(newname);
-      free(newtmpl);
+    }
 
+    if ( status != LCFG_STATUS_ERROR ) {
+
+      /* Full template */
+      char * newtmpl = strdup(token);
+
+      if ( !lcfgtemplate_set_tmpl( new_template, newtmpl ) ) {
+        status = invalid_template( msg, "bad value '%s'", newtmpl );
+        free(newtmpl);
+
+      }
+    }
+
+    if ( status == LCFG_STATUS_ERROR ) {
+      lcfgtemplate_destroy(new_template);
+      new_template = NULL;
       break;
     }
 
-    if ( head_template == NULL ) {
+    /* Stash the templates */
+
+    if ( head_template == NULL )
       head_template = new_template;
-    } else {
+    else
       prev_template->next = new_template;
-    }
+
+    /* Prepare for next loop */
+
     prev_template = new_template;
 
     token = strtok_r( NULL, whitespace, &saveptr );
@@ -236,35 +341,36 @@ LCFGStatus lcfgtemplate_from_string( const char * input,
 
   free(remainder);
 
-  if ( !ok ) {
+  if ( status == LCFG_STATUS_ERROR ) {
     lcfgtemplate_destroy(head_template);
     head_template = NULL;
   }
 
   *result = head_template;
 
-  return ( ok ? LCFG_STATUS_OK : LCFG_STATUS_ERROR );
+  return status;
 }
 
 LCFGTemplate * lcfgtemplate_find( const LCFGTemplate * head_template,
                                   const char * field_name ) {
+  assert( field_name != NULL );
 
-  LCFGTemplate * template = NULL;
-  size_t field_len = strlen(field_name);
+  size_t field_len = isempty(field_name) ? 0 : strlen(field_name);
 
-  LCFGTemplate * cur_template = (LCFGTemplate *) head_template;
-  while ( cur_template != NULL ) {
+  LCFGTemplate * result = NULL;
 
-    if ( field_len == cur_template->name_len &&
-         strncmp( field_name, cur_template->name, field_len ) == 0 ) {
-      template = cur_template;
-      break;
-    }
+  const LCFGTemplate * cur_tmpl = NULL;
+  for ( cur_tmpl = head_template;
+        cur_tmpl != NULL && result == NULL;
+        cur_tmpl = cur_tmpl->next ) {
 
-    cur_template = cur_template->next;
+    if ( lcfgtemplate_is_valid(cur_tmpl) &&
+         field_len == cur_tmpl->name_len &&
+         strncmp( field_name, cur_tmpl->name, field_len ) == 0 )
+      result = (LCFGTemplate *) cur_tmpl;
   }
 
-  return template;
+  return result;
 }
 
 /* eof */
