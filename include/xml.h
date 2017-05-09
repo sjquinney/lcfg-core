107be24df877d1520380257f5c5a146dc21c61c0
squinney@INF.ED.AC.UK <squinney@INF.ED.AC.UK@c6336a39-18d7-4234-9af0-a2bff563b40a>
2017-05-09 12:58:05 +0000

    Updated function specs. Removed a few which are static
    git-svn-id: https://svn.lcfg.org/svn/source/trunk/lcfg-core@32660 c6336a39-18d7-4234-9af0-a2bff563b40a

--- include/xml.h
+++ include/xml.h
@@ -23,12 +23,13 @@
 #define LCFGXML_ATTRVALUE_DEPTH 3
 
 xmlTextReaderPtr lcfgxml_init_reader( const char * filename, char ** msg );
+void lcfgxml_end_reader(xmlTextReaderPtr reader);
 
 LCFGStatus lcfgxml_collect_metadata( xmlTextReaderPtr reader,
 				     const  char * target_nodename,
 				     LCFGProfile * profile,
 				     char ** msg )
-  __attribute__((nonnull (1,2,3))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgprofile_from_xml( const char   * filename,
 				 LCFGProfile ** profile,
@@ -44,13 +45,13 @@ LCFGStatus lcfgprofile_overrides_xmldir( LCFGProfile  * profile,
                                          const char   * override_dir,
                                          const LCFGContextList * ctxlist,
                                          char        ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgprofile_overrides_context( LCFGProfile * profile,
 					  const char * override_dir,
                                           LCFGContextList * ctxlist,
                                           char       ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 #define LCFGXML_TOP_NODE             "lcfg"
 #define LCFGXML_COMPS_PARENT_NODE    "components"
@@ -58,35 +59,15 @@ LCFGStatus lcfgprofile_overrides_context( LCFGProfile * profile,
 #define LCFGXML_PACKAGES_PARENT_NODE "packages"
 #define LCFGXML_PACKAGES_CHILD_NODE  "package"
 
-bool lcfgxml_moveto_next_tag( xmlTextReaderPtr reader )
-  __attribute__((nonnull (1)));
+bool lcfgxml_moveto_next_tag( xmlTextReaderPtr reader );
 
 bool lcfgxml_moveto_node( xmlTextReaderPtr reader,
-                          const char * target_nodename )
-  __attribute__((nonnull (1,2)));
+                          const char * target_nodename );
 
 bool lcfgxml_correct_location( xmlTextReaderPtr reader,
-                               const char * expected_nodename )
-  __attribute__((nonnull (1,2)));
+                               const char * expected_nodename );
 
-void lcfgxml_set_error_message( char ** msg, const char * fmt, ...);
-
-LCFGStatus lcfgxml_gather_attributes( xmlTextReaderPtr reader,
-				      LCFGResource * head_res,
-				      const char * compname,
-				      char ** msg )
-  __attribute__((nonnull (1,2,3))) __attribute__((warn_unused_result));
-
-LCFGStatus lcfgxml_process_record( xmlTextReaderPtr reader,
-				   LCFGComponent * lcfgcomp,
-				   const LCFGTemplate * templates,
-				   char ** thistag,
-				   const LCFGTagList * ancestor_tags,
-				   const char * base_context,
-				   const char * base_derivation,
-				   const LCFGContextList * ctxlist,
-				   char ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+LCFGStatus lcfgxml_error( char ** msg, const char * fmt, ...);
 
 LCFGStatus lcfgxml_process_resource( xmlTextReaderPtr reader,
 				     LCFGComponent * lcfgcomp,
@@ -97,7 +78,7 @@ LCFGStatus lcfgxml_process_resource( xmlTextReaderPtr reader,
 				     const char * base_derivation,
 				     const LCFGContextList * ctxlist,
 				     char ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgxml_process_component( xmlTextReaderPtr reader,
 				      const char * compname,
@@ -106,7 +87,7 @@ LCFGStatus lcfgxml_process_component( xmlTextReaderPtr reader,
 				      const char * base_derivation,
 				      const LCFGContextList * ctxlist,
 				      char ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgxml_process_components( xmlTextReaderPtr reader,
 				       LCFGComponentList ** result,
@@ -115,14 +96,14 @@ LCFGStatus lcfgxml_process_components( xmlTextReaderPtr reader,
 				       const LCFGContextList * ctxlist,
 				       const LCFGTagList * comps_wanted,
 				       char ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgxml_process_package( xmlTextReaderPtr reader,
 				    LCFGPackage **result,
 				    const char * base_context,
 				    const char * base_derivation,
 				    char ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 LCFGStatus lcfgxml_process_packages( xmlTextReaderPtr reader,
 				     LCFGPackageList ** active,
@@ -131,7 +112,7 @@ LCFGStatus lcfgxml_process_packages( xmlTextReaderPtr reader,
 				     const char * base_derivation,
 				     const LCFGContextList * ctxlist,
 				     char ** msg )
-  __attribute__((nonnull (1))) __attribute__((warn_unused_result));
+  __attribute__((warn_unused_result));
 
 #endif /* LCFG_CORE_XML_H */
 
