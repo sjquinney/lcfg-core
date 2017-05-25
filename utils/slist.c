5027b4aee90b1ed3812f68d812fa174abbd132f6
squinney@INF.ED.AC.UK <squinney@INF.ED.AC.UK@c6336a39-18d7-4234-9af0-a2bff563b40a>
2017-05-25 13:49:47 +0000

    Completed docs
    git-svn-id: https://svn.lcfg.org/svn/source/trunk/lcfg-core@32924 c6336a39-18d7-4234-9af0-a2bff563b40a

--- utils/slist.c
+++ utils/slist.c
@@ -1,5 +1,5 @@
 /**
- * @file utils/utils.c
+ * @file utils/slist.c
  * @brief Generic single-linked list support
  * @author Stephen Quinney <squinney@inf.ed.ac.uk>
  * @copyright 2014-2017 University of Edinburgh. All rights reserved. This project is released under the GNU Public License version 2.
@@ -12,6 +12,24 @@
 #include <assert.h>
 
 #include "utils.h"
+
+/**
+ * @brief Create and initialise a new list node
+ *
+ * This creates a simple wrapper @c LCFGSListNode node which is used
+ * to hold a pointer to an item in a list.
+ *
+ * If the memory allocation for the new structure is not successful
+ * the @c exit() function will be called with a non-zero value.
+ *
+ * To avoid memory leaks, when the new structure is no longer required
+ * the @c lcfgslistnode_destroy() function should be called.
+ *
+ * @param[in] data Pointer to list item
+ *
+ * @return Pointer to new @c LCFGSListNode
+ *
+ */
 
 LCFGSListNode * lcfgslistnode_new( void * data ) {
   assert( data != NULL );
@@ -29,6 +47,24 @@ LCFGSListNode * lcfgslistnode_new( void * data ) {
 
   return node;
 }
+
+/**
+ * @brief Destroy a resource node
+ *
+ * When the specified @c LCFGSListNode is no longer required this can
+ * be used to free all associated memory.
+ *
+ * Note that destroying an @c LCFGSListNode does not destroy the
+ * associated data for the list item, that must be done separately.
+ *
+ * If the value of the pointer passed in is @c NULL then the function
+ * has no affect. This means it is safe to call with a pointer to a
+ * node which has already been destroyed (or potentially was never
+ * created).
+ *
+ * @param[in] node Pointer to @c LCFGSListNode to be destroyed.
+ *
+ */
 
 void lcfgslistnode_destroy(LCFGSListNode * node) {
 
